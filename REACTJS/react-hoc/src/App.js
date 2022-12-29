7b3c5982b674aac9f55c97d5a2103c9661e6ded2
sriramchinnu <banothsriram351@gmail.com>
2022-12-29 23:59:31 +0530

    task cmplted

---
+++ REACTJS/react-hoc/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Click from './component/Click';
+import Hover from './component/Hover';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Click/>
+            <hr/>
+            <Hover/>
+            
+          
+    </div>
+  );
+}
+
+export default App;
